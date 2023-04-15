4e55f9f588a8dd91a93586e769b0215525047317
Lucjan Konopka <111157625+lucjankonopka@users.noreply.github.com>
2023-04-15 14:41:56 +0200

    Complete work added: code plus results from GCS bucket

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='rides_counter',
+    version='1.0',
+    install_requires=[
+      'apache-beam[gcp]==2.33.0'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    description='Coding Challenge'
+)
